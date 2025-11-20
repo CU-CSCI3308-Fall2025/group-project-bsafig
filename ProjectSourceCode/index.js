0027e5aa7d8653397306f33205dc402ca12fd0d2
James Nguyen <jamesnguyendw@gmail.com>
2025-11-20 16:30:19 -0700

    render index.js change for lab 12

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -50,7 +50,7 @@ app.use(session({
 
 // Database configuration
 const dbConfig = {
-    host: 'db',
+    host: process.env.HOST,
     port: 5432,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
